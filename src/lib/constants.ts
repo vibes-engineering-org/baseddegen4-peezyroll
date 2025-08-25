682fdc301a84cf8fca027936c257bac4687e5fa2
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-08-25 14:04:11 +0000

    Code generation: 8f3bec6e

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_TITLE = "PeezyRoll";
-export const PROJECT_DESCRIPTION = "by @baseddegen4";
+export const PROJECT_DESCRIPTION = "Roll dice and place bets in this classic street craps game";
 export const PROJECT_CREATOR = "baseddegen4";
 export const PROJECT_AVATAR_URL =
   "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
